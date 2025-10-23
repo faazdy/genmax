ccd29a87ec2d63d406a0c9ab659c5b678ae9e68d
faazdy <leorengifor@gmail.com>
2025-10-22 23:40:26 -0500

    fix: route of hero video

--- src/App.jsx
+++ src/App.jsx
@@ -40,7 +40,7 @@ function App() {
         </section>
 
         <section className="hero-video">
-          <video src="./present.mp4" autoPlay muted controls></video>
+          <video src="./presentVideo.mp4" autoPlay muted controls></video>
         </section>
         <hr />
         <section className="glow-up-changes">
